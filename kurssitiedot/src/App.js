1b9b8f1fa6da6eb53fbfb4db79777d2abdada27b
Lefti90 <55137561+Lefti90@users.noreply.github.com>
2022-07-06 18:10:45 +0300

    2.5 done

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -1,33 +1,4 @@
-const Part = (props) =>{
-  const total = props.parts.reduce((previousValue, currentValue) => {
-  return previousValue + currentValue.exercises
-},0)
-  return(
-    <div>
-        {props.parts.map(part =>
-          <p key={part.id}>
-            {part.name} {part.exercises}
-          </p>)}
-          <b>Total of {total} exercises</b>
-    </div>
-  )
-}
-
-const Courses = (props) => {
-  return(
-  <div>   
-      {props.courses.map(course => {        
-        return (
-          <div key={course.id}>
-            <h2 key={course.id}>{course.name}</h2>
-            <Part parts={course.parts}/>
-          </div>
-        )      
-      })
-      }
-  </div>
-  )
-}
+import Courses from "./components/Courses"
 
 const App = () => {
 
